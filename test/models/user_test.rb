43cc99418572c83fe1abf9ebc5fd6c81973a5dcd
Luis Silva <silmar.dll@gmail.com>
2025-03-24 23:29:48 -0600

    task: enable Minitest::Spec::DSL

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,23 +3,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def test_normalize_email_address
-    user = build(:user, email_address: ' maIL@mail.com')
+  let(:user) { build(:user) }
 
-    assert_equal 'mail@mail.com', user.email_address
+  test 'builds a valid user' do
+    assert user.valid?
   end
 
-  def test_validate_email_address_presence
-    user = build(:user, email_address: nil)
+  describe '#email_address' do
+    test 'normalizes the email address' do
+      user.email_address = ' maIL@mail.com'
 
-    assert user.invalid?
+      assert_equal 'mail@mail.com', user.email_address
+    end
+
+    test 'requires presence' do
+      user.email_address = nil
+
+      assert user.invalid?
+    end
+
+    test 'requires uniqueness' do
+      create(:user, email_address: 'mail@mail.com')
+      user.email_address = 'mail@mail.com'
+
+      assert user.invalid?
+      assert_includes user.errors.details[:email_address], error: :taken, value: 'mail@mail.com'
+    end
   end
 
-  def test_validate_email_address_uniqueness
-    create(:user, email_address: 'mail@mail.com')
-    user = build(:user, email_address: 'mail@mail.com')
+  test 'requires a password' do
+    user.password = nil
 
     assert user.invalid?
-    assert_includes user.errors.details[:email_address], error: :taken, value: 'mail@mail.com'
   end
 end
