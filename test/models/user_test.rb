f2b31ab37e82cbfd20139bee88b4538b3228fb78
Luis Silva <silmar.dll@gmail.com>
2025-03-24 22:43:48 -0600

    task: add email_address presence and uniqueness validations for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_normalize_email_address
+    user = build(:user, email_address: ' maIL@mail.com')
+
+    assert_equal 'mail@mail.com', user.email_address
+  end
+
+  def test_validate_email_address_presence
+    user = build(:user, email_address: nil)
+
+    assert user.invalid?
+  end
+
+  def test_validate_email_address_uniqueness
+    create(:user, email_address: 'mail@mail.com')
+    user = build(:user, email_address: 'mail@mail.com')
+
+    assert user.invalid?
+    assert_includes user.errors.details[:email_address], error: :taken, value: 'mail@mail.com'
+  end
 end
