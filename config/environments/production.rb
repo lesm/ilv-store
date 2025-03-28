3c0e8008270da2c3b8acc0cb88f2e176cbbb5d8e
Luis Silva <silmar.dll@gmail.com>
2025-03-28 10:51:32 -0600

    task: Setup smtp for Sendgrid account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,17 +59,23 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: 'example.com' }
-
-  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_options = {
+    from: 'silmar.vim@gmail.com',
+    reply_to: 'silmar.vim@gmail.com'
+  }
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch('HOST', 'silmardll.com')
+  }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 2525,
+    domain: ENV.fetch('DOMAIN', 'silmardll.com'),
+    user_name: Rails.application.credentials.dig(:smtp, :user_name),
+    password: Rails.application.credentials.dig(:smtp, :password),
+    authentication: 'plain',
+    enable_starttls: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
