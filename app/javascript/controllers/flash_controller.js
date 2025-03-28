f7b84ee8f7c935e4727a85c5e35d9e605c525327
Luis Silva <silmar.dll@gmail.com>
2025-03-28 08:59:43 -0600

    [IS-7] Add flash partial to render flash messages (#7)
    * feat: add flash partial to render flash messages
    
    add animation to the flash messages
    add missing flash messages in registration and session controllers
    
    * task: remove unnecessary ID for flash messages

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    this.timeoutId = setTimeout(() => {
+      this.element.remove();
+    }, 9200);
+  }
+
+  close() {
+    clearTimeout(this.timeoutId);
+    this.element.remove();
+  }
+}
