e51ffb91e8b1a9928424447cc025887edf2db55e
Luis Silva <silmar.dll@gmail.com>
2025-04-09 19:15:06 -0600

    [IS-27] fix: calculate cart subtotal
    Update view to take content from translations

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -3,9 +3,18 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  let(:cart) { build(:cart) }
+  let(:cart) { create(:cart) }
 
   test 'builds a valid cart' do
-    assert cart.valid?
+    assert build(:cart).valid?
+  end
+
+  describe '#total_price' do
+    test 'calculates the total price of the cart' do
+      cart.items << build(:cart_item, product: build(:product, price: 10), quantity: 2)
+      cart.items << build(:cart_item, product: build(:product, price: 5), quantity: 1)
+
+      assert_equal 25, cart.total_price
+    end
   end
 end
