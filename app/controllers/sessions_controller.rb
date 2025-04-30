ee375dfe889e79766d915377729c4a7f30e397f7
Luis Silva <silmar.dll@gmail.com>
2025-04-30 14:02:06 -0600

    task: add sessions translations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,9 +11,9 @@ class SessionsController < ApplicationController
   def create
     if (user = User.authenticate_by(params.permit(:email_address, :password)))
       start_new_session_for user
-      redirect_to after_authentication_url, notice: 'Welcome back!'
+      redirect_to after_authentication_url, notice: t('.success')
     else
-      redirect_to new_session_path, alert: 'Try another email address or password.'
+      redirect_to new_session_path, alert: t('.failure')
     end
   end
 
