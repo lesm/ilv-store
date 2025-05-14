99b274ec58e26f54acea2ecb6d3f62b34e0a2809
Luis Silva <silmar.dll@gmail.com>
2025-05-14 08:26:54 -0600

    [IS-32] Feat: fill address fields based on postal code

--- app/javascript/controllers/addresses/details_controller.js
+++ app/javascript/controllers/addresses/details_controller.js
@@ -3,10 +3,6 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static targets = ['compact', 'expanded']
 
-  connect() {
-    console.log('Details controller connected');
-  }
-
   toggle() {
     this.compactTarget.classList.toggle('hidden')
     this.expandedTarget.classList.toggle('hidden')
