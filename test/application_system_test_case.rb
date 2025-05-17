9538338507d4b80833615e782ba7308febf19488
Luis Silva <silmar.dll@gmail.com>
2025-05-16 19:29:17 -0600

    task: clear rails cache in system test to avoid rate limit in tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,4 +4,8 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  setup do
+    Rails.cache.clear
+  end
 end
