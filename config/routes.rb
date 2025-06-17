b912cdcd0de9ab8441fc9b092f22e328d12fbbc2
Luis Silva <silmar.dll@gmail.com>
2025-06-17 08:14:17 -0600

    task: create routes/application.rb file to wrap routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,27 +12,7 @@ Rails.application.routes.draw do
   mount MissionControl::Jobs::Engine, at: '/jobs'
 
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
-    resource :session, only: %i[new create destroy]
-    resource :registration, only: %i[new create]
-    resources :passwords, only: %i[new create edit update], param: :token
-
-    resources :products
-    resources :addresses
-
-    resource :cart, only: %i[show] do
-      resources :items, only: %i[create update destroy], controller: 'carts/items'
-    end
-
-    resources :orders, only: %i[index new create]
-
-    scope module: :orders, path: :order, as: :order do
-      resources :addresses, only: %i[index update]
-    end
-
-    resource :user, only: %i[] do
-      resource :theme_preference, only: %i[update], controller: 'users/theme_preference'
-      resource :email_verification, only: %i[show], controller: 'users/email_verifications'
-    end
+    draw :application
   end
 
   namespace :backoffice do
