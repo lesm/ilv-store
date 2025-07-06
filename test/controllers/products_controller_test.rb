f3b1d478c776f83804bcbb54298917e7111f9e78
Luis Silva <silmar.dll@gmail.com>
2025-07-06 16:33:02 -0600

    [IS-80] add product's show view (and visual improvements) (#116)
    * [IS-80] feat: display product details in a drawer
    
    * task: add visual improvements
    
    * chage the amounts color to text-magenta-500
    * add product image in the cart
    * change background color of the application layout to bg-blue-50
    
    * task: add form_quantity partial
    
    * task: homologate all buttons and add show order link
    
    * task: homologate padding for index views (Orders and Addresses)

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,9 +3,23 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  let(:user) { create(:user, :with_cart) }
+
   describe '#index' do
     test 'returns success response' do
+      create_list(:product, 3)
+
       get products_url
+
+      assert_response :success
+    end
+  end
+
+  describe '#show' do
+    before { authenticate_as(user) }
+
+    test 'returns success response' do
+      get product_url(id: create(:product).id, turbo_frame: 'drawer')
 
       assert_response :success
     end
