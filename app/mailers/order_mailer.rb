78396755e8b6a9e1dba82a451196f6b6b9af0f04
Luis Silva <silmar.dll@gmail.com>
2025-07-21 21:59:34 -0600

    task: user the product cover in the email for the created order

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class OrderMailer < ApplicationMailer
+  helper :application
+
   def created
     @order = params[:order]
     mail subject: t('.subject'), to: email_address_with_name(@order.user.email, @order.user.name)
