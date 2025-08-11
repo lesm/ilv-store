0302054b012ea1c57042560e66da7b27f109c9a2
Luis Silva <silmar.dll@gmail.com>
2025-08-11 14:20:20 -0600

    feat: display item error in cart and order new
    item quantity is greater than product stock

--- test/models/cart/item_test.rb
+++ test/models/cart/item_test.rb
@@ -3,21 +3,40 @@
 require 'test_helper'
 
 class CartItemTest < ActiveSupport::TestCase
-  let(:cart_item) { build(:cart_item) }
+  let(:item) { build(:cart_item) }
 
   test 'builds a valid cart item' do
-    assert cart_item.valid?
+    assert item.valid?
   end
 
   describe '#quantity' do
     test 'valid when is greater than 0' do
-      cart_item.quantity = 2
-      assert cart_item.valid?
+      item.quantity = 2
+      assert item.valid?
     end
 
     test 'invalid when is less than 1' do
-      cart_item.quantity = 0
-      assert cart_item.invalid?
+      item.quantity = 0
+      assert item.invalid?
+    end
+  end
+
+  describe '#stock_availability' do
+    let(:product) { create(:product, stock: 5) }
+
+    before do
+      item.product = product
+    end
+
+    test 'valid when stock is sufficient' do
+      item.quantity = 3
+      assert item.valid?
+    end
+
+    test 'invalid when stock is insufficient' do
+      item.quantity = 10
+      assert item.invalid?
+      assert_includes item.errors[:quantity], 'Solo quedan 5 disponibles.'
     end
   end
 end
