fb80f6ce6ccec210b2279886fe478abd75f019d8
Luis Silva <silmar.dll@gmail.com>
2025-09-23 21:06:21 -0600

    task: skip load seed for Country::State if there are any

--- db/seeds/states.rb
+++ db/seeds/states.rb
@@ -2,6 +2,8 @@
 
 country = Country.find_or_create_by!(name: 'México', code: 'MX')
 
+return if country.states.any?
+
 Country::MX_STATES.each do |state_name|
   Country::State.find_or_create_by!(
     name: state_name[:name], code: state_name[:code], country:
