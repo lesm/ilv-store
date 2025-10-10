790017f4ce7de462f03842acb544deb44ffdc808
Luis Silva <silmar.dll@gmail.com>
2025-10-09 21:08:43 -0600

    [IS-108] task: add infinite scroll in products view (#202)
    * task: add infinite scroll in products view
    
    * task: add tests for products pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,17 @@ class ProductsController < ApplicationController
   allow_unauthenticated_access only: %i[index show]
 
   def index
-    @products = Product.includes(:translations, cover_attachment: :blob)
-                       .where(translations: { locale: I18n.locale })
+    # Use joins instead of includes to avoid DISTINCT issues with Pagy
+    products_query = Product.joins(:translations)
+                            .where(translations: { locale: I18n.locale })
+                            .includes(:translations, cover_attachment: :blob)
+
+    @pagy, @products = pagy(products_query, limit: 8)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 
   def show
