a78ffe69707ec02dd720894724d44da16c44a822
Luis Silva <silmar.dll@gmail.com>
2025-11-19 13:47:14 -0600

    chore: use Bullet.add_safelist for ActiveStorage::Attachment
    because since the blob is used in the helper (view) bullet can not dectec the usage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,10 @@ Rails.application.configure do
     Bullet.console       = true
     Bullet.rails_logger  = true
     Bullet.add_footer    = true
+
+    # Allowlist for associations used in view helpers/partials
+    # These are intentionally eager-loaded but Bullet can't detect usage in helpers
+    Bullet.add_safelist type: :unused_eager_loading, class_name: 'ActiveStorage::Attachment', association: :blob
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
