afa72915dc37d416888b63c589bffe93721c083f
Luis Silva <silmar.dll@gmail.com>
2026-01-30 09:04:11 -0600

    chore: set Mexican time zone

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module IlvStore
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Mexico City'
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
